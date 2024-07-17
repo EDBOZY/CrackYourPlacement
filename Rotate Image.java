a07b83755b77455db0d86a136ebd8dd7e62e7618
Ben Tom Abey <127616692+EDBOZY@users.noreply.github.com>
2024-07-17 14:05:53 +0530

    Create Rotate Image.java

---
+++ Rotate Image.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+            }
+        }
+        for(int i=0;i<n;i++){
+            int l=0;
+            int r=n-1;
+            while(l<=r){
+                int temp=matrix[i][l];
+                matrix[i][l]=matrix[i][r];
+                matrix[i][r]=temp;
+                l++;
+                r--;
+            }
+        }
+        
+    }
+}
