6702728088741f49c35c771c51e7bcfaa7777b56
Ben Tom Abey <127616692+EDBOZY@users.noreply.github.com>
2024-07-20 14:06:30 +0530

    Create Subsets.java

---
+++ Subsets.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>>a=new ArrayList<>();
+        List<Integer>b=new ArrayList<>();
+        Arrays.sort(nums);
+        check(a,b,nums,0);
+        return a;
+    }
+    public void check(List<List<Integer>>a,List<Integer>b,int[]nums,int i){
+        if(!a.contains(new ArrayList<>(b))){
+            a.add(new ArrayList<>(b));
+        }
+        if(i==nums.length){
+            return;
+        }
+        b.add(nums[i]);
+        check(a,b,nums,i+1);
+        b.remove(b.size()-1);
+        check(a,b,nums,i+1);
+    }
+}
